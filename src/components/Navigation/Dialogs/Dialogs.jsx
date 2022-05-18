38ed02d7ed25c56e0d8169b1eb6f4ddf593fc29a
sinoptik77 <illyap2000@gmail.com>
2022-05-18 15:35:51 +0300

    23

--- src/components/Navigation/Dialogs/Dialogs.jsx
+++ src/components/Navigation/Dialogs/Dialogs.jsx
@@ -1,8 +1,31 @@
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    return <div>
+        <NavLink to={'/dialogs/' + props.id}
+                 className={(dialog) => (dialog.isActive ? s.active : s.item)}>{props.name}</NavLink>
+    </div>
+}
+const Message = (props) => {
+    return <div className= {s.item}>
+        <div>{props.message}</div>
+    </div>
+}
 const Dialogs = (props) => {
     return <div className={s.dialogs}>
-        Dialogs
+        <div className={s.dialogItem}>
+            <DialogItem id='1' name='Nastya'/>
+            <DialogItem id='2' name='Vlad'/>
+            <DialogItem id='3' name='Zhenya'/>
+            <DialogItem id='4' name='Misha'/>
+            <DialogItem id='5' name='Sasha'/>
+        </div>
+        <div className={s.messageItem}>
+            <Message message='Yo'/>
+            <Message message='Hi, how a u?'/>
+            <Message message='Wasup?'/>
+        </div>
     </div>
 }
 
